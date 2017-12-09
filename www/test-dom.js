bf23f4c905f8d791ff5d7d5c28d62c5e2fee276c
Nic Ferrier <nic@ferrier.me.uk>
2017-12-09 01:00:41 +0000

    homogenize the test data

--- www/test-dom.js
+++ www/test-dom.js
@@ -13,13 +13,13 @@ const { document } = (new JSDOM(`
             <form>
                 <textarea name="import" placeholder="data import">
 
-Date, Type, Description, Value, Balance, Account Name, Account Number
+Date, Type, Description, Value, Balance, Account Name
 
-31/12/2014,D/D,"'WWF UK",-10.00,1781.21,"'FERRIER NJ","'600008-49127454",
-31/12/2014,POS,"'4545 30DEC14 , WWW.MEMSET.COM/REC, CRANLEIGH GB",-8.64,1791.21,"'FERRIER NJ","'600008-49127454",
-30/12/2014,D/D,"'EE AND T-MOBILE",-51.95,1799.85,"'FERRIER NJ","'600008-49127454",
-29/12/2014,D/D,"'SCOTTISHPOWER",-228.00,1851.80,"'FERRIER NJ","'600008-49127454",
-29/12/2014,D/D,"'ORANGE",-12.31,2079.80,"'FERRIER NJ","'600008-49127454",
+31/12/2010,D/D,"PAYEE #1",-10.00,1781.21,"'FERRIER NJ",
+31/12/2010,POS,"'PAYEE #2, LONG 1213, SOMEWHERE GB",-8.64,1791.21,"'FERRIER NJ",
+30/12/2010,D/D,"UPHONEUPOO",-51.95,1799.85,"'FERRIER NJ",
+29/12/2010,D/D,"BIGLECCY",-228.00,1851.80,"'FERRIER NJ",
+29/12/2010,D/D,"OCHRE",-12.31,2079.80,"'FERRIER NJ",
 
                 </textarea>
                 <button name="import!">import</button>
